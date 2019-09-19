8e58a0e3f0432b7b1ac40c955d717862020ef44c
Andres Ramirez Quiros <andrew.rq+github@gmail.com>
2019-09-18 22:36:36 -0600

    Big changes, read description
    express added
    CORS added
    refactor to some functions
    update added to send json

--- ServerApp/app.js
+++ ServerApp/app.js
@@ -1,13 +1,15 @@
 //  include the http and url module
-const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
 const exec = require('child_process').exec;
+const express = require('express');
+const app = express();
 
 //  var for the user
 const hostname = '0.0.0.0';
 const port = 8080;
+const pathController = '../GPIO/build/house_controller';
 
 let ledDining = false;
 let ledKitchen = false;
@@ -15,42 +17,27 @@ let ledLiving = false;
 let ledBed1 = false;
 let ledBed2 = false;
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/', (req, res) => {
+  param(req, res);
+});
+
+app.get('/image', (req, res) => {
+  res.status(200);
+  image(req, res);
+});
+
 //  create the http server accepting requests to port 8080
-http.createServer((req, res) => {
-  const urlParsed = url.parse(req.url);
-  const pathArray = urlParsed.pathname.split('/'); // First & last items empty
-
-  //  direct the request to appropriate function
-  if (pathArray[1] === 'image') { //  If ascking for image, process
-    image(req, res);
-  } else { // Else, show homepage
-    homepage(req, res);
-  }
-}).listen(port, hostname);
-
-console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => `Server running at http://${hostname}:${port}/`);
 
 /**
- * Function that returns the homepage
- * @param {*} req: request parameter
- * @param {*} res: response parameter
- */
-function homepage(req, res) {
-  //  use the url to parse the requested url and get the led number
-  const query = url.parse(req.url, true).query;
-  const ledRoom = query.led;
-
-  if (typeof ledRoom !== 'undefined') { //  Led is in the parameters
-    console.log('Led parameter detected');
-    switchLed(ledRoom);
-  }
-
-  res.writeHead(400, {'Content-type': 'text/plain'});
-  res.end('Hello, this is the home page :)');
-}
-
-/**
- * Function that returns the homepage
+ * Function that returns the param
  * Query example: http://0.0.0.0:8080/image/
  * @param {*} req: request parameter
  * @param {*} res: response parameter
@@ -73,12 +60,145 @@ async function image(req, res) {
 }
 
 /**
- * Function that toggles the led of a room
- * It can also toggle the led values by using:
- * http://0.0.0.0:8080/?led=kitchen
- * @param {*} room: name of the room to toggle the led
+ * Function that returns the param
+ * It can also toggle the param values by using:
+ * http://0.0.0.0:8080/?param=kitchen
+ * @param {*} req: request parameter
+ * @param {*} res: response parameter
  */
-function switchLed(room) {
+function param(req, res) {
+  //  use the url to parse the requested url and get the param number
+  const query = url.parse(req.url, true).query;
+  const ledRoom = query.led;
+  const update = query.update;
+
+  if (typeof ledRoom !== 'undefined') { //  Led is in the parameters
+    console.log('Led parameter detected');
+    detectRoom(req, res, ledRoom);
+  } else if (typeof update !== 'undefined') {
+    updateData(req, res);
+  } else {
+    console.log('Invalid param');
+    res.sendStatus(400);
+  }
+}
+
+/**
+ * Function that sends a JSON with last values
+ * @param {*} req: request parameter
+ * @param {*} res: response parameter
+ */
+function updateData(req, res) {
+  //  const returnedArray = updateDoors(req, res);
+
+  res.json({
+    'bed1': ledBed1,
+    'bed2': ledBed2,
+    'kitchen': ledKitchen,
+    'living': ledLiving,
+    'dining': ledDining,
+    //  'door1': returnedArray[0],
+    //  'door2': returnedArray[1],
+    //  'door3': returnedArray[2],
+    //  'door4': returnedArray[3],
+    'door1': false,
+    'door2': true,
+    'door3': false,
+    'door4': false,
+  });
+}
+
+/**
+ * Function that gets the values of the doors
+ * @param {*} req: request parameter
+ * @param {*} res: response parameter
+ * @return {Array} returnedArray
+ */
+function updateDoors(req, res) {
+  const returnedArray = [];
+
+  //  First door
+  let dir = exec(`${pathController} -d 0`,
+      (error, stdout, stderr) => {
+        console.log(error);
+        console.log(stdout);
+      }
+  );
+  dir.on('exit', (code) => {
+    if (code === 1) {
+      returnedArray.push(true);
+    } else if (code === 0) {
+      returnedArray.push(false);
+    } else {
+      returnedArray.push(false);
+      console.log('Unexpected -1 in door0');
+    }
+  });
+
+  //  Second door
+  dir = exec(`${pathController} -d 1`,
+      (error, stdout, stderr) => {
+        console.log(error);
+        console.log(stdout);
+      }
+  );
+  dir.on('exit', (code) => {
+    if (code === 1) {
+      returnedArray.push(true);
+    } else if (code === 0) {
+      returnedArray.push(false);
+    } else {
+      returnedArray.push(false);
+      console.log('Unexpected -1 in door1');
+    }
+  });
+
+  //  Third door
+  dir = exec(`${pathController} -d 2`,
+      (error, stdout, stderr) => {
+        console.log(error);
+        console.log(stdout);
+      }
+  );
+  dir.on('exit', (code) => {
+    if (code === 1) {
+      returnedArray.push(true);
+    } else if (code === 0) {
+      returnedArray.push(false);
+    } else {
+      returnedArray.push(false);
+      console.log('Unexpected -1 in door2');
+    }
+  });
+
+  //  Third door
+  dir = exec(`${pathController} -d 3`,
+      (error, stdout, stderr) => {
+        console.log(error);
+        console.log(stdout);
+      }
+  );
+  dir.on('exit', (code) => {
+    if (code === 1) {
+      returnedArray.push(true);
+    } else if (code === 0) {
+      returnedArray.push(false);
+    } else {
+      returnedArray.push(false);
+      console.log('Unexpected -1 in door3');
+    }
+  });
+
+  return returnedArray;
+}
+
+/**
+ * Function that detects the room in the request
+ * @param {*} req: request parameter
+ * @param {*} res: response parameter
+ * @param {*} room: name of the room to toggle the param
+ */
+function detectRoom(req, res, room) {
   let roomBool;
   let roomNumber;
 
@@ -114,12 +234,28 @@ function switchLed(room) {
       break;
   }
 
+  switchLed(req, res, roomNumber, roomBool);
+}
+
+/**
+ * Function that toggles the param of a room
+ * @param {*} req:
+ * @param {*} res:
+ * @param {*} roomNumber:
+ * @param {*} roomBool:
+ */
+function switchLed(req, res, roomNumber, roomBool) {
   if (roomNumber === -1) { // Check if invalid room
     console.log('Invalid room');
+    res.sendStatus(400);
   } else { // Executes the script
     console.log('Room number: ', roomNumber, roomBool);
-    exec(`/user/bin/house_controller -l ${roomNumber} ${roomBool}`,
-        (error, stdout, stderr) => {});
+
+    exec(`${pathController} -l ${roomNumber} ${roomBool}`,
+        (error, stdout, stderr) => {}
+    );
+
+    res.sendStatus(200);
   }
 }
 
