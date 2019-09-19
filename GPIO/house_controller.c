7236b2726008e82888d2b683f461e795e69c56a6
Ignacio Mora <ignacimo98@hotmail.com>
2019-09-18 18:16:55 -0600

    Fixed the string comparison

--- GPIO/house_controller.c
+++ GPIO/house_controller.c
@@ -5,26 +5,26 @@ int main(int argc, char **argv) {
     int pin_number;
     int pin_mode;
     if (argc == 4) {
-        if (argv[1] == "-l") {
+        if (strcmp(argv[1] , "-l") == 0) {
             int to_write;
             pin_mode = 0;  // out
-            if (argv[2] == "0") {
+            if (strcmp(argv[2], "0") == 0) {
                 pin_number = 5;
-            } else if (argv[2] == "1") {
+            } else if (strcmp(argv[2], "1") == 0) {
                 pin_number = 6;
-            } else if (argv[2] == "2") {
+            } else if (strcmp(argv[2], "2") == 0) {
                 pin_number = 13;
-            } else if (argv[2] == "3") {
+            } else if (strcmp(argv[2], "3") == 0) {
                 pin_number = 19;
-            } else if (argv[2] == "4") {
+            } else if (strcmp(argv[2], "4") == 0) {
                 pin_number = 26;
             } else {
                 printf("There are only 5 lights ranging from index 0 to 4");
                 return -1;
             }
-            if (argv[3] == "0") {
+            if (strcmp(argv[3], "0") == 0) {
                 to_write = 0;
-            } else if (argv[3] == "1") {
+            } else if (strcmp(argv[3], "1") == 0) {
                 to_write = 1;
             } else {
                 printf("Only 0 and 1 are valid states");
@@ -38,16 +38,16 @@ int main(int argc, char **argv) {
             return 0;
         }
     } else if (argc == 3) {
-        if (argv[1] == "-d") {
+        if (strcmp(argv[1] , "-d") == 0) {
             int to_return;
             pin_mode = 1;  // in
-            if (argv[2] == "0") {
+            if (strcmp(argv[2], "0") == 0) {
                 pin_number = 12;
-            } else if (argv[2] == "1") {
+            } else if (strcmp(argv[2], "1") == 0) {
                 pin_number = 16;
-            } else if (argv[2] == "2") {
+            } else if (strcmp(argv[2], "2") == 0) {
                 pin_number = 20;
-            } else if (argv[2] == "3") {
+            } else if (strcmp(argv[2], "3") == 0) {
                 pin_number = 21;
             } else {
                 printf("There are only 4 doors ranging from index 0 to 3");
